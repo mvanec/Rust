7c9ae2055c6647e04c9edb86555bc1d0c80c68cc
mattvanecek <matt@mattvanecek.com>
2024-06-18 23:37:44 -0500

    6.1

--- Chapter06/6.1.Defining_Enums/src/main.rs
+++ Chapter06/6.1.Defining_Enums/src/main.rs
@@ -1,10 +1,24 @@
 #![allow(dead_code)]
 #![allow(unused_variables)]
 
-fn foo(x: &i32) {
-    println!("{x}");
-}
 fn main() {
-    let x = null;
-    foo(x);
+    enum IpAddrKind {
+        V4,
+        V6,
+    }
+
+    struct IpAddr {
+        kind: IpAddrKind,
+        address: String,
+    }
+
+    let home = IpAddr {
+        kind: IpAddrKind::V4,
+        address: String::from("127.0.0.1"),
+    };
+
+    let loopback = IpAddr {
+        kind: IpAddrKind::V6,
+        address: String::from("::1"),
+    };
 }
