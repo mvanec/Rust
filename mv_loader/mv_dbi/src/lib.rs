cf7a08433477cfeb7bcacec18725a077b629703e
mvanec <112896707+mvanec@users.noreply.github.com>
2024-07-31 23:32:49 -0500

    Trying to add a fetch method

--- mv_loader/mv_dbi/src/lib.rs
+++ mv_loader/mv_dbi/src/lib.rs
@@ -1,5 +1,6 @@
 use chrono::NaiveDate;
 use chrono::NaiveDateTime;
+use sqlx::Row;
 use sqlx::{AnyConnection, Connection, Error};
 // use sqlx::types::chrono::{NaiveDate, NaiveDateTime};
 
@@ -55,9 +56,32 @@ impl Database {
                 }
             }
         }
-        eprintln!("Database is {}", self.conn.backend_name());
+
         let result = query.execute(&mut self.conn).await?;
         Ok(result.rows_affected())
+    }
+
+    pub async fn fetch(&mut self, sql: &str, data: &[DataVariant]) -> Result<u64, Error> {
+        let mut query = sqlx::query(sql);
+        for param in data {
+            match param {
+                DataVariant::Int(value) => query = query.bind(value),
+                DataVariant::String(value) => query = query.bind(value),
+                DataVariant::Float(value) => query = query.bind(value),
+                DataVariant::Date(value) => {
+                    let date_str = value.to_string(); // Convert NaiveDate to String
+                    query = query.bind(date_str);
+                }
+                DataVariant::DateTime(value) => {
+                    let datetime_str = value.to_string(); // Convert NaiveDateTime to String
+                    query = query.bind(datetime_str);
+                }
+            }
+        }
+        let result = query.fetch_one(&mut self.conn).await?;
+        let col_count: u64 = result.len() as u64;
+        eprintln!("row = {:?}", result.columns());
+        Ok(col_count)
     }
 }
 
@@ -90,5 +114,30 @@ mod tests {
         assert_eq!(rows_affected, 1);
 
         Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_database_select() -> Result<(), Error> {
+        let config = DbConfig::new("sqlite::memory:");
+        let mut db = Database::new(config).await?;
+
+        db.create_table().await?;
+        setup(&mut db).await?;
+
+        let data = [DataVariant::String("Some data".to_string())];
+        let sql = "SELECT * FROM my_table WHERE data = ?";
+        let rows_affected = db.fetch(sql, &data).await?;
+
+        assert_eq!(rows_affected, 1);
+
+        Ok(())
+    }
+
+    async fn setup(db: &mut Database) -> Result<u64, Error> {
+        let data = [DataVariant::String("Some data".to_string()),
+                                      DataVariant::Date(chrono::Utc::now().date_naive())];
+        let sql = "INSERT INTO my_table (data, created_at) VALUES (?, ?)";
+        let rows_affected = db.insert(sql, &data).await?;
+        Ok(rows_affected)
     }
 }
