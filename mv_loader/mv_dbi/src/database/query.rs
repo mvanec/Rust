05ec56f5ad244e2cdc3e8b2e6a0e1a4c1ed977ca
mvanec <112896707+mvanec@users.noreply.github.com>
2024-08-06 01:17:56 -0500

    Reworking the DbiDatabase

--- mv_loader/mv_dbi/src/database/query.rs
+++ mv_loader/mv_dbi/src/database/query.rs
@@ -2,10 +2,21 @@ use sqlx::query::Query;
 use sqlx::sqlite::SqliteArguments;
 use sqlx::sqlite::Sqlite;
 use sqlx::Error;
+use sqlx::Pool;
 use sqlx::sqlite::SqliteRow;
 
 pub trait ToQuery {
     fn to_execute_query(&self) -> Query<Sqlite, SqliteArguments>;
     fn to_fetch_query(&self) -> Query<Sqlite, SqliteArguments>;
     fn from_row(&mut self, row: &SqliteRow) -> Result<(), Error>;
+}
+
+
+#[allow(dead_code)]
+pub trait DbObject<DB, T>
+where DB: sqlx::Database
+{
+    async fn insert_one(pool: &Pool<DB>, dbo: &T) -> Result<u64, Error>;
+    async fn retrieve_one(&self, pool: &Pool<DB>) -> Result<T, Error>;
+    async fn retrieve_all(&self, pool: &Pool<DB>) -> Result<Vec<T>, Error>;
 }
--- mv_loader/mv_dbi/src/lib.rs
+++ mv_loader/mv_dbi/src/lib.rs
@@ -1,13 +1,23 @@
 mod database;
 mod model;
 
+#[cfg(test)]
+use database::query::DbObject;
+
+use model::project::Project;
+use sqlx::Error;
+use sqlx::Pool;
 use sqlx::Row;
-use sqlx::SqliteConnection;
-use sqlx::{Connection, Error};
+use sqlx::Sqlite;
 // use sqlx::types::chrono::{NaiveDate, NaiveDateTime};
 
 use database::query::ToQuery;
-use model::project;
+
+pub enum DataObject<'a> {
+    Project(&'a Project),
+    #[cfg(test)]
+    MyTable(&'a tests::MyTable)
+}
 
 #[derive(Debug)]
 pub struct DbConfig {
@@ -23,13 +33,13 @@ impl DbConfig {
 }
 
 pub struct DbiDatabase {
-    conn: SqliteConnection,
+    pool: Pool<Sqlite>,
 }
 
 impl DbiDatabase {
     pub async fn new(config: DbConfig) -> Result<Self, Error> {
-        let conn = SqliteConnection::connect(&config.url).await?;
-        Ok(Self { conn })
+        let pool = Pool::connect(&config.url).await?;
+        Ok(Self { pool: pool })
     }
 
     pub async fn create_table(&mut self) -> Result<(), Error> {
@@ -40,22 +50,29 @@ impl DbiDatabase {
                 created_at DATE NOT NULL
             )",
         )
-        .execute(&mut self.conn)
+        .execute(&self.pool)
         .await?;
         Ok(())
     }
 
     pub async fn insert_query(&mut self, item: &impl ToQuery) -> Result<u64, Error> {
         let query = item.to_execute_query();
-        let result = query.execute(&mut self.conn).await?;
+        let result = query.execute(&self.pool).await?;
         Ok(result.rows_affected())
+    }
+
+    pub async fn do_insert(&mut self, data_object: &DataObject<'_>) -> Result<u64, Error> {
+        match data_object {
+            DataObject::Project(_) => todo!(),
+            #[cfg(test)]
+            DataObject::MyTable(value) => <tests::MyTable>::insert_one(&self.pool, &value).await,
+        }
     }
 
     pub async fn fetch(&mut self, item: &mut impl ToQuery) -> Result<u64, Error> {
         let query = item.to_fetch_query();
-        let result = query.fetch_one(&mut self.conn).await?;
+        let result = query.fetch_one(&self.pool).await?;
         let col_count: u64 = result.len() as u64;
-        eprintln!("row = {:?}", result.columns());
         let _ = item.from_row(&result);
         Ok(col_count)
     }
@@ -65,15 +82,16 @@ impl DbiDatabase {
 mod tests {
     use super::*;
     use chrono::NaiveDate;
-    use sqlx::Error;
+    use database::query::DbObject;
+    use serde::{Deserialize, Serialize};
     use sqlx::query::Query;
     use sqlx::sqlite::Sqlite;
     use sqlx::sqlite::SqliteArguments;
     use sqlx::sqlite::SqliteRow;
-    use sqlx::Row;
     use sqlx::Column;
-    use serde::{Deserialize, Serialize};
-
+    use sqlx::Error;
+    use sqlx::Pool;
+    use sqlx::Row;
 
     #[tokio::test]
     async fn test_database_select() -> Result<(), Error> {
@@ -112,6 +130,25 @@ mod tests {
         Ok(())
     }
 
+    #[tokio::test]
+    async fn test_dbobject_insert_one() -> Result<(), Error> {
+        let config = DbConfig::new("sqlite::memory:");
+        let mut db = DbiDatabase::new(config).await?;
+        db.create_table().await?;
+        let mt = MyTable {
+            id: 0,
+            data: "Testing Insert".to_string(),
+            created_at: NaiveDate::MAX,
+        };
+        let dao = DataObject::MyTable(&mt);
+        let result = db.do_insert(&dao).await?;
+        assert_eq!(result, 1);
+        
+        let inserted = MyTable::insert_one(&db.pool, &mt).await?;
+        assert_eq!(inserted, 1);
+        Ok(())
+    }
+
     async fn setup(db: &mut DbiDatabase) -> Result<MyTable, Error> {
         let my_table = MyTable::default();
 
@@ -121,13 +158,13 @@ mod tests {
                     return Err(Error::Protocol("Insert returned 0 rows affected".into()));
                 }
                 Ok(my_table)
-            },
-            Err(error) => Err(error)
+            }
+            Err(error) => Err(error),
         }
     }
 
     #[derive(Debug, Clone, Default, PartialEq, Deserialize, Serialize)]
-    struct MyTable {
+    pub struct MyTable {
         id: i64,
         data: String,
         created_at: NaiveDate,
@@ -165,6 +202,26 @@ mod tests {
 
             Ok(())
         }
+    }
 
+    impl DbObject<Sqlite, MyTable> for MyTable {
+        async fn insert_one(pool: &Pool<Sqlite>, dbo: &MyTable) -> Result<u64, Error> {
+            let query_str = "INSERT INTO my_table (data, created_at) VALUES (?, ?)";
+
+            let result = sqlx::query(query_str)
+                .bind(dbo.data.clone())
+                .bind(dbo.created_at)
+                .execute(pool)
+                .await?;
+            Ok(result.rows_affected())
+        }
+
+        async fn retrieve_one(&self, pool: &Pool<Sqlite>) -> Result<MyTable, Error> {
+            todo!()
+        }
+
+        async fn retrieve_all(&self, pool: &Pool<Sqlite>) -> Result<Vec<MyTable>, Error> {
+            todo!()
+        }
     }
 }
